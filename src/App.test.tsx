656a8a9c322a60c9c017543696a6fe71215d81bd
Imran Khan <imransilvake@gmail.com>
2023-03-24 11:37:14 +0100

    refactor: Code

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,3 +1,5 @@
+import App from './App';
+
 describe('App Test', () => {
 	test('Test1', () => {
 		expect(2).toBe(2);
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import './App.scss';
 
-import { useState } from 'react';
-
 import reactLogo from '/assets/svg/react.svg';
 import viteLogo from '/assets/svg/vite.svg';
 
-function App() {
-	const [count, setCount] = useState(0);
-
+const App = () => {
 	return (
 		<div className="App">
 			<div>
@@ -19,14 +15,7 @@ function App() {
 				</a>
 			</div>
 			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
 		</div>
 	);
-}
+};
 export default App;
