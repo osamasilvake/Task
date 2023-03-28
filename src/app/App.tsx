bc3899bdee931b35748c47e15c38e9b24eae367d
Imran Khan <imransilvake@gmail.com>
2023-03-28 17:10:21 +0200

    refactor: Code

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,7 @@
-import Logo2 from '@assets/svg/react.svg';
-import Logo from '@assets/svg/vite.svg';
-
-import Demo from '@/Demo';
-
 const App = () => {
 	return (
 		<div className="App">
-			<h1>App</h1>
-			<Demo />
-
-			<img src={Logo} alt="Logo" />
-
-			<img src={Logo2} alt="Logo2" />
+			<h1>Robot GUI</h1>
 		</div>
 	);
 };
--- src/app/Demo.tsx
+++
@@ -1,8 +0,0 @@
-const Demo = () => {
-	return (
-		<div className="Demo">
-			<h1>Demo</h1>
-		</div>
-	);
-};
-export default Demo;
