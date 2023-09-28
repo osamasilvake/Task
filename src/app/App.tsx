f82249d6ac75215ef373d1c205a8a65f007bbd1b
osamasilvake <muhammadosamavu@gmail.com>
2023-09-28 20:35:55 +0500

    deleted main branch

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 const App = () => {
 	return (
 		<div className="App">
+			{/* i have deleted main brach scuccessfully */}
 			<h1>Hey Robot GUI {import.meta.env.VITE_NAME}</h1>
 			<p>hello</p>
 		</div>
