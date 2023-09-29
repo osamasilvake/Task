40332f66254638c37b873ad71bae5cb34f7e8e3d
Muhammad osama <mosama007pak@gmail.com>
2023-09-30 00:57:25 +0500

    adit env files

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,14 @@
 const App = () => {
-	// let a;
 	return (
 		<div className="App">
 			{/* i have deleted main brach scuccessfully */}
 			<h1>Hey Robot GUI {import.meta.env.VITE_NAME}</h1>
-			<p>hello g</p>
+			<p> i am {import.meta.env.VITE_API_URL}</p>
+			{/* note: jab aap yarn build karte hain toh production build, .env.production 
+			ke values ko istemal karta hai aur
+			production-ready code generate hota hai. beacuse development 
+			 chala rahe hote hain kyunki development
+			server .env.development ko follow karta hai. */}
 		</div>
 	);
 };
