ef85d18eb31ce11cda3ec7efe3e8bf51c939ac1b
Muhammad osama <mosama007pak@gmail.com>
2023-10-01 23:18:56 +0500

    added app.1.tsx added .env.development

--- src/app/App.1.tsx
+++ src/app/App.1.tsx
@@ -1,16 +1,16 @@
 export const App = () => {
-	// const items = ['item1', 'item2', 'item3'];
+	const items = ['item1', 'item2', 'item3'];
 
 	return (
 		<div className="App">
 			{/* i have deleted main brach scuccessfully */}
 			<h1>Hey Robot GUI {import.meta.env.VITE_NAME}</h1>
 			<p> hello {import.meta.env.VITE_API_URL}</p>
-			{/* <ul>
+			<ul>
 				{items.map((item, index) => (
 					<li>{item}</li>
 				))}
-			</ul> */}
+			</ul>
 			{/* note: jab aap yarn build karte hain toh production build, .env.production
             ke values ko istemal karta hai aur
             production-ready code generate hota hai. beacuse development
