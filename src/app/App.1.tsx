9505ca8dfaa569e8f8e752a193ab120f969ff63f
Muhammad osama <mosama007pak@gmail.com>
2023-10-03 12:48:20 +0500

    cheking hasky

--- src/app/App.1.tsx
+++ src/app/App.1.tsx
@@ -1,17 +1,16 @@
 export const App = () => {
-	const items = ['item1', 'item2', 'item3'];
+	// const items = ['item1', 'item2', 'item3'];
 
 	return (
 		<div className="App">
-			{/* i have deleted main brach scuccessfully */}
 			<h1>Hey Robot GUI {import.meta.env.VITE_NAME}</h1>
 			<p> hello {import.meta.env.VITE_API_URL}</p>
-			<ul>
+			{/* <ul>
 				{items.map((item, index) => (
 					<li key={index}>{item}</li>
 				))}
 				;
-			</ul>
+			</ul> */}
 			{/* note: jab aap yarn build karte hain toh production build, .env.production
             ke values ko istemal karta hai aur
             production-ready code generate hota hai. beacuse development
@@ -23,6 +22,11 @@ export const App = () => {
 
 // What are Git Hooks?
 // Git hooks are scripts that you can set up to run at certain
-// events in the Git lifecycle. These events include different
-// stages of a commit, like before a commit (pre-commit) and after
-//  a commit (post-commit).
+// events in the Git lifecycle.
+///////////////////////////////
+// "lint-staged": {
+//   "*.{js,jsx,ts,tsx,json}": [
+//     "lint:es",
+//     "pretty:check"
+//   ]
+// }
