bd05834e67e12fa3d7e74535f051bb4dd27d407c
Muhammad osama <mosama007pak@gmail.com>
2023-10-03 22:16:57 +0500

    added: App.tsx eslintrc App.1.tsx

--- src/app/App.1.tsx
+++ src/app/App.1.tsx
@@ -1,36 +1,8 @@
 export const App = () => {
-	// const items = ['item1', 'item2', 'item3'];
-	let a;
 	return (
 		<div className="App">
 			<h1>Hey Robot GUI {import.meta.env.VITE_NAME}</h1>
 			<p> hello {import.meta.env.VITE_API_URL}</p>
-			{/* <ul>
-				{items.map((item, index) => (
-					<li key={index}>{item}</li>
-				))}
-				;
-			</ul> */}
 		</div>
 	);
 };
-
-// 	 note: jab aap yarn build karte hain toh production build, .env.production
-// ke values ko istemal karta hai aur
-// production-ready code generate hota hai. beacuse development
-//  chala rahe hote hain kyunki development
-// server .env.development ko follow karta hai.
-
-// What are Git Hooks?
-// Git hooks are scripts that you can set up to run at certain
-// events in the Git lifecycle.
-///////////////////////////////
-// inside pkg.json
-//   "lint-staged": {
-//     "src/**/*.{js,html.css}": "prettier --write",
-//     "src/**/*.js": "eslint"
-//   }
-// inside pre-commit file
-// npx lint-staged
-
-// why eslint: we want some rules regurading or code
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,35 @@
 const App = () => {
-	// const items = ['item1', 'item2', 'item3'];
-
+	const items = ['item1', 'item2', 'item3'];
 	return (
 		<div className="App">
-			{/* i have deleted main brach scuccessfully */}
 			<h1>Hey Robot GUI {import.meta.env.VITE_NAME}</h1>
-			<p> i am {import.meta.env.VITE_API_URL}</p>
-			{/* <ul>
-				{items.map((item, index) => (
-					<li>{item}</li>
+			<p> hallo {import.meta.env.VITE_API_URL}</p>
+			<ul>
+				{items.map((item) => (
+					<li key={item}>{item}</li>
 				))}
-			</ul> */}
-
-			{/* note: jab aap yarn build karte hain toh production build, .env.production 
-			ke values ko istemal karta hai aur
-			production-ready code generate hota hai. beacuse development 
-			 chala rahe hote hain kyunki development
-			server .env.development ko follow karta hai. */}
+			</ul>
 		</div>
 	);
 };
 export default App;
+
+// 	 note: jab aap yarn build karte hain toh production build, .env.production
+// ke values ko istemal karta hai aur
+// production-ready code generate hota hai. beacuse development
+//  chala rahe hote hain kyunki development
+// server .env.development ko follow karta hai.
+
+// What are Git Hooks?
+// Git hooks are scripts that you can set up to run at certain
+// events in the Git lifecycle.
+///////////////////////////////
+// inside pkg.json
+//   "lint-staged": {
+//     "src/**/*.{js,html.css}": "prettier --write",
+//     "src/**/*.js": "eslint"
+//   }
+// inside pre-commit file
+// npx lint-staged
+
+// why eslint: we want some rules regurading or code
