b9bec302111068baf149c3aba48d5eb552a8511b
Muhammad osama <mosama007pak@gmail.com>
2023-10-03 14:50:05 +0500

    added file

--- src/app/App.1.tsx
+++ src/app/App.1.tsx
@@ -1,6 +1,6 @@
 export const App = () => {
 	// const items = ['item1', 'item2', 'item3'];
-
+	let a;
 	return (
 		<div className="App">
 			<h1>Hey Robot GUI {import.meta.env.VITE_NAME}</h1>
@@ -11,22 +11,26 @@ export const App = () => {
 				))}
 				;
 			</ul> */}
-			{/* note: jab aap yarn build karte hain toh production build, .env.production
-            ke values ko istemal karta hai aur
-            production-ready code generate hota hai. beacuse development
-             chala rahe hote hain kyunki development
-            server .env.development ko follow karta hai. */}
 		</div>
 	);
 };
+
+// 	 note: jab aap yarn build karte hain toh production build, .env.production
+// ke values ko istemal karta hai aur
+// production-ready code generate hota hai. beacuse development
+//  chala rahe hote hain kyunki development
+// server .env.development ko follow karta hai.
 
 // What are Git Hooks?
 // Git hooks are scripts that you can set up to run at certain
 // events in the Git lifecycle.
 ///////////////////////////////
-// "lint-staged": {
-//   "*.{js,jsx,ts,tsx,json}": [
-//     "lint:es",
-//     "pretty:check"
-//   ]
-// }
+// inside pkg.json
+//   "lint-staged": {
+//     "src/**/*.{js,html.css}": "prettier --write",
+//     "src/**/*.js": "eslint"
+//   }
+// inside pre-commit file
+// npx lint-staged
+
+// why eslint: we want some rules regurading or code
