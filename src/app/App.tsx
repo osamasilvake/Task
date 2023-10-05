6c3b399fd81139c56f9bb6ccee2b5b271531a4bb
Muhammad osama <mosama007pak@gmail.com>
2023-10-05 22:25:33 +0500

    added file

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,96 @@
+import { useEffect, useState } from 'react';
+
 const App = () => {
 	const items = ['item1', 'item2', 'item3'];
-	console.log('hello');
-	let a;
+	const [count, setCount] = useState(0);
+	// console.log(count);
+	// let a;
+	useEffect(() => {
+		document.title = `Count: ${count}`;
+	}, [count]);
+
+	/*eslint no-ternary: "error"*/
+	// const value1 = 5;
+	// const value2 = 10;
+	// const value3 = 20;
+	// const result = condition1 ? value1 : condition2 ? value2 : condition3 ? value3 : defaultValue;
+	// console.log(result);
+
+	// let result;
+	// const value1 = 5;
+	// const value2 = 10;
+	// const value3 = 20;
+	// if (condition1) {
+	// 	result = value1;
+	// } else if (condition2) {
+	// 	result = value2;
+	// } else if (condition3) {
+	// 	result = value3;
+	// } else {
+	// 	result = defaultValue;
+	// }
+	////////////////////////
+	// function getGreeting(timeOfDay: string): string {
+	// 	return timeOfDay === 'morning' ? 'Good morning!' : 'Hello!';
+	// }
+	// const time = 'morning';
+	// const greeting = getGreeting(time);
+	// console.log(greeting);
+
+	// function getGreeting(timeOfDay: string): string {
+	// 	if (timeOfDay === 'morning') {
+	// 		return 'Good morning!';
+	// 	} else {
+	// 		return 'Hello!';
+	// 	}
+	// }
+	// const time = 'morning';
+	// const greeting = getGreeting(time);
+	// console.log(greeting);
+
+	/////////////////////////////////////
+	// reuire await
+	// async function fetchData() {
+	// 	// Missing await here
+	// 	const res = await fetch('https://url');
+	// 	const data = res.json();
+	// 	console.log(data);
+	// }
+
+	// fetchData();
+	///////
+	// async function fetchData() {
+	// 	// Missing await here
+	// 	fetch('https://api.example.com/data')
+	// 		.then((response) => response.json())
+	// 		.then((data) => {
+	// 			console.log(data);
+	// 		});
+	// }
+
+	// fetchData();
+	//////////////////////////////////
+	// "arrow-body-style": ["error", "as-needed"],
+
+	// type FooReturnType = {
+	// 	bar: {
+	// 		foo: number;
+	// 		bar: number;
+	// 	};
+	// };
+	// const foo: () => number = () => 0;
+	// const foo2: () => FooReturnType = () => ({
+	// 	bar: {
+	// 		foo: 1,
+	// 		bar: 2
+	// 	}
+	// });
+	// console.log(foo()); // Outputs: 0
+	// console.log(foo2().bar.foo); // Outputs: 1
+	// console.log(foo2().bar.bar); // Outputs: 2
+	//////////
+	// "@typescript-eslint/no-empty-function": "off",
+	// const foo = () => {};
 
 	return (
 		<div className="App">
@@ -12,6 +101,9 @@ const App = () => {
 					<li key={item}>{item}</li>
 				))}
 			</ul>
+			<button onClick={() => setCount(count + 1)}>Increment</button>
+			{/* <input type="text" > */}
+			{/* <button></button> */}
 		</div>
 	);
 };
@@ -36,3 +128,5 @@ export default App;
 // npx lint-staged
 
 // why eslint: we want some rules regurading or code
+/////////////////
+// release-it tool and is used to automate the release process of your software.
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import './i18n/i18n';
 import '@assets/scss/main.scss';
 import '@assets/fonts/index';
-
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
 
 import App from '@/App';
-
 // import App from '@/App.1';
 
 const container = document.getElementById('root') as HTMLElement;
