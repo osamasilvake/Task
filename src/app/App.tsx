cda71aae01ead7df827429284bb9cead5635ab8f
Muhammad osama <mosama007pak@gmail.com>
2023-10-17 16:55:42 +0500

    style: A html css

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,17 @@
 import '../assets/scss/main.scss';
 
 const App = () => (
-	<div>
-		<h1>form</h1>
+	<div className=" bg-cyan-500">
+		<form>
+			<h1>Login</h1>
+			<p>Please enter your credentials to login</p>
+			<input type="text" placeholder="username" />
+			<input type="password" placeholder="password" />
+			<button>Login</button>
+			<p>
+				Not registred?<a href="">Create any account</a>
+			</p>
+		</form>
 	</div>
 );
 export default App;
