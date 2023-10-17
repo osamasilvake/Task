80a1aaba5247e0180593663617c8e9caef36cf1d
Muhammad osama <mosama007pak@gmail.com>
2023-10-17 19:59:00 +0500

    feat: Add login

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,19 @@
 import '../assets/scss/main.scss';
 
 const App = () => (
-	<div className=" bg-cyan-500">
-		<form>
-			<h3>Login</h3>
-			<p>Please enter your credentials to login</p>
-			<input type="text" placeholder="username" />
-			<input type="password" placeholder="password" />
-			<button>Login</button>
-			<p>
-				Not registred?<a href="">Create any account</a>
-			</p>
-		</form>
+	<div className="flex justify-center items-center h-screen">
+		<div className="bg-white w-96 flex m-2 sm:m-0">
+			<form className="flex flex-col py-10 px-4 mx-auto">
+				<h2 className="text-center pb-5 text-3xl">Login</h2>
+				<p className="text-center pb-6">Please enter your credentials to login</p>
+				<input type="text" placeholder="username" className="border-0 py-3 bg-slate-200" />
+				<input type="password" placeholder="password" className="mt-4 border-0 py-3 bg-slate-200" />
+				<button className=" bg-green-800 text-white py-3 mt-4">Login</button>
+				<p className="text-center pt-4">
+					Not registred?<a href="">Create any account</a>
+				</p>
+			</form>
+		</div>
 	</div>
 );
 export default App;
