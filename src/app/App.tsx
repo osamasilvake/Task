c8a93784fd897c0b38722a0ba04f6f2e8e813ec5
Muhammad osama <mosama007pak@gmail.com>
2023-10-18 12:57:16 +0500

    refactor: Code

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,8 @@
-import '../assets/scss/main.scss';
+import Loginform from './Loginform';
 
 const App = () => (
-	<div className="flex justify-center items-center h-screen">
-		<div className="bg-white w-96 flex m-2 sm:m-0">
-			<form className="flex flex-col py-10 px-4 mx-auto">
-				<h2 className="text-center pb-5 text-3xl">Login</h2>
-				<p className="text-center pb-6">Please enter your credentials to login</p>
-				<input type="text" placeholder="username" className="border-0 py-3 bg-slate-200" />
-				<input type="password" placeholder="password" className="mt-4 border-0 py-3 bg-slate-200" />
-				<button className="bg-gray-600 text-white py-3 mt-4">Login</button>
-				<p className="text-center pt-4">
-					Not registred?<a href="">Create any account</a>
-				</p>
-			</form>
-		</div>
+	<div>
+		<Loginform />
 	</div>
 );
 export default App;
