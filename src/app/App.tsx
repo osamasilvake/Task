7fe573650354a6a7bb833f3e62e9d82ab2a37588
Muhammad osama <mosama007pak@gmail.com>
2023-10-18 11:58:56 +0500

    refactor: Css code

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,7 +8,7 @@ const App = () => (
 				<p className="text-center pb-6">Please enter your credentials to login</p>
 				<input type="text" placeholder="username" className="border-0 py-3 bg-slate-200" />
 				<input type="password" placeholder="password" className="mt-4 border-0 py-3 bg-slate-200" />
-				<button className="bg-green-800 text-white py-3 mt-4">Login</button>
+				<button className="bg-gray-600 text-white py-3 mt-4">Login</button>
 				<p className="text-center pt-4">
 					Not registred?<a href="">Create any account</a>
 				</p>
